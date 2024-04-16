226b5c02592384dbc13815b36645bbea44ffde80
Danny Ghrist <danielghrist@gmail.com>
2024-04-16 16:42:55 -0500

    HW16: Initial upload

---
+++ hw16/movies.php
@@ -0,0 +1,71 @@
+  <!-- MOVIES CONTENT -->
+  <section id="testimonial">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-sm-12">
+          <div class="section-title">
+            <h2>Movies<small>i love the smell of nostalgia and escapism</small></h2>
+          </div>
+          <div class="p-left">
+            <h2> Favorite Movies:
+            </h2>
+            <p> I really can't pick a favorite, plus there are so much more, but here are a few of my favorite movies.
+            </p>
+            <p> A few of my favorite movies:
+            </p>
+            <ol>
+              <li><i>Star Wars</i></li>
+              <li><i>Pretty much all the Marvel movies</i></li>
+              <li><i>Equilibrium</i></li>
+              <li><i>Requiem For a Dream</i></li>
+              <li><i>Animated Disney movies:</i>
+                <ol type="i">
+                  <li><i>Aladdin</i></li>
+                  <li><i>The Little Mermaid</i></li>
+                  <li><i>Beauty and the Beast</i></li>
+                  <li><i>The Lion King</i></li>
+                  <li><i>Hunchback of Notre Dame</i></li>
+                </ol>
+              </li>
+              <li><i>The Whale</i></li>
+            </ol>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maxime deserunt fuga, non
+              praesentium dolore totam itaque eius? Corrupti vitae natus, deleniti laudantium deserunt error? Eligendi
+              magnam sed velit maxime. Optio omnis, enim itaque iusto nulla ipsa vel quasi error nostrum aspernatur
+              dolore ad eaque magnam quod rem. Veniam, aspernatur minima. Quasi aperiam earum odit quos ut possimus
+              aliquam consectetur.
+            </p>
+            <p> Dicta corporis optio sint autem consequuntur obcaecati, mollitia, assumenda nobis ipsa similique
+              perspiciatis accusamus voluptatibus suscipit blanditiis. Officiis eum tenetur eveniet harum. Saepe, eius!
+              Reprehenderit modi ex nulla reiciendis iure.
+            </p>
+            <p> Quos omnis magni quisquam, obcaecati deserunt ullam. Tenetur nesciunt rerum harum sequi earum quaerat
+              corrupti vitae magni minima? Cumque nostrum, repellendus totam incidunt culpa nam omnis voluptates
+              provident in odit. Deleniti rem quos exercitationem soluta neque sit voluptatem repellat facere sint? Enim
+              optio, quam nisi vero qui illum delectus animi tempore amet exercitationem distinctio odit recusandae?
+              Dicta atque molestiae optio.
+            </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia nobis perspiciatis, nisi maiores
+              praesentium deserunt ipsum labore quisquam excepturi, molestias sunt? Odit eum facere nemo modi
+              asperiores? Repudiandae, eligendi minus.
+            </p>
+            <p> Dicta, atque neque. Quidem ut voluptates reiciendis sint. Quisquam rem illum, nam voluptate totam at
+              dolorem minima. Harum, officiis dignissimos ex, earum aut consequuntur, veritatis labore doloribus fugit
+              adipisci maiores! Quos, tempore explicabo accusamus similique obcaecati natus architecto magni, quisquam
+              id tenetur magnam, libero iste cum pariatur dolores soluta dignissimos ut! Ab aspernatur odio perferendis
+              dignissimos iure adipisci suscipit aliquam.
+            </p>
+            <p> Enim voluptatem tempora labore, debitis tempore eius quis cum vel non dolores reiciendis odit natus
+              dolorem quam. Magni corrupti harum quas atque explicabo, delectus, eveniet officia dolorem quasi ullam
+              laboriosam. Voluptates praesentium quas voluptate autem doloribus reiciendis iste totam quia alias, porro
+              repudiandae fugit ea consequuntur deserunt debitis exercitationem libero. Corrupti, repudiandae
+              asperiores? Aut vero labore, dolore error maiores ex!
+            </p>
+          </div>
+          <hr>
+        </div>
+
+      </div>
+    </div>
+  </section>
---
+++ hw16/navigation.php
@@ -0,0 +1,51 @@
+<!-- Dynamic Menu Links -->
+<?php
+
+echo '<div class="collapse navbar-collapse">';
+echo '<ul class="nav navbar-nav navbar-nav-first">';
+
+if (!isset($_GET['page'])) {
+  $page = 'home';
+} else {
+  $page = $_GET['page'];
+}
+
+if ($page == 'home') {
+  echo '<li class="active"><a href="index.php" class="smoothScroll">Home</a></li>';
+} else {
+  echo '<li><a href="index.php" class="smoothScroll">Home</a></li>';
+}
+if ($page == 'work') {
+  echo '<li class="active"><a href="index.php?page=work" class="smoothScroll">Work</a></li>';
+} else {
+  echo '<li><a href="index.php?page=work" class="smoothScroll">Work</a></li>';
+}
+if ($page == 'school') {
+  echo '<li class="active"><a href="index.php?page=school" class="smoothScroll">School</a></li>';
+} else {
+  echo '<li><a href="index.php?page=school" class="smoothScroll">School</a></li>';
+}
+if ($page == 'hobbies') {
+  echo '<li class="active"><a href="index.php?page=hobbies" class="smoothScroll">Hobbies</a></li>';
+} else {
+  echo '<li><a href="index.php?page=hobbies" class="smoothScroll">Hobbies</a></li>';
+}
+if ($page == 'movies') {
+  echo '<li class="active"><a href="index.php?page=movies" class="smoothScroll">Movies</a></li>';
+} else {
+  echo '<li><a href="index.php?page=movies" class="smoothScroll">Movies</a></li>';
+}
+if ($page == 'contact') {
+  echo '<li class="active"><a href="index.php?page=contact" class="smoothScroll">Contact</a></li>';
+} else {
+  echo '<li><a href="index.php?page=contact" class="smoothScroll">Contact</a></li>';
+}
+
+
+echo '</ul>';
+echo '<ul class="nav navbar-nav navbar-right">';
+echo '<li><img src="assets/images/your_image.png" class="me-img" alt="Simpsonized Version of Myself" title="Simpson Me"></i></li>';
+echo '</ul>';
+echo '</div>';
+
+?>
